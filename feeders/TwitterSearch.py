85dbe736f0c0b9abe18c20da454e6f5cb513f81f
Bryan <bryan@hottub-collector1.(none)>
2014-03-21 13:29:57 -0400

    Iterate through search terms and display

--- feeders/TwitterSearch.py
+++ feeders/TwitterSearch.py
@@ -23,11 +23,35 @@ from twython import Twython
 config = SafeConfigParser()
 config.read('../config/config.ini')
 
+search_terms = config.get('twitter','keywords')
+search_terms_list = search_terms.split(',')
+
 twitter = Twython(config.get('twitter','consumer_key'), config.get('twitter','consumer_secret'), config.get('twitter','access_token_key'), config.get('twitter','access_token_secret'))
 
-search = twitter.search(q='bryanbrannigan',until='2013-11-30')
+for term in search_terms_list:
+	search = twitter.search(q=term)
 
-for message in search['statuses']:
-	print message.get('text')
+	for message in search['statuses']:
+		keywords = config.get('twitter','keywords')
+                r_keywords = keywords.split(",")
+                r_pattern = '(^|)'
+                for word in r_keywords:
+                        r_pattern += '(' + word + ')|'
+                r_pattern += ' ( |$)'
+                matches = re.search(r_pattern,message.get('text'), re.I|re.M)
+                if matches:
+                        print 'Matched: %s' % matches.group()
+                        print 'User: %s' % message['user'].get('screen_name')
+                        print 'User ID: %s' % message['user'].get('id_str')
+                        print 'Got tweet with text: %s' % message.get('text')
+                        print 'Tweet ID: %s' % message.get('id')
+                        for mention in message['entities']['user_mentions']:
+                                print 'Mentioned: %s' %mention.get('screen_name')
+                                print 'Mentioned ID: %s' % mention.get('id_str')
+                        for hashtag in message['entities']['hashtags']:
+                                print 'Hashtag: %s' % hashtag.get('text')
+                        for url in message['entities']['urls']:
+                                print 'URL: %s' % url.get('expanded_url')
+                        print 'Timestamp: %s' % message.get('created_at')
 
-print twitter.get_lastfunction_header('x-rate-limit-remaining')
+	print twitter.get_lastfunction_header('x-rate-limit-remaining')
