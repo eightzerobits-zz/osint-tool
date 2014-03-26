f7eb54ca0591d4e1e42ca2dabe96ea6362fa65db
Bryan <bryan@hottub-collector1.(none)>
2014-03-26 13:58:08 -0400

    Updated SQL

--- sql/twitter.sql
+++ sql/twitter.sql
@@ -20,13 +20,13 @@ ENGINE = InnoDB;
 -- Table `hottub`.`twitter_tweets`
 -- -----------------------------------------------------
 CREATE  TABLE IF NOT EXISTS `hottub`.`twitter_tweets` (
-  `idtwitter_tweets` INT NOT NULL ,
-  `twitter_tweets_id` BIGINT UNSIGNED NULL ,
-  `twitter_tweetscol` VARCHAR(45) NULL ,
-  `idtwitter_users` BIGINT UNSIGNED NULL ,
+  `idtwitter_tweets` BIGINT UNSIGNED NOT NULL ,
+  `idtwitter_users` BIGINT NULL ,
   `twitter_tweets_text` VARCHAR(255) NULL ,
   `twitter_tweets_timestamp` VARCHAR(45) NULL ,
   `twitter_tweets_coordinates` VARCHAR(255) NULL ,
+  `twitter_tweets_retweeted` TINYINT(1) NULL ,
+  `twitter_tweets_retweet+count` INT NULL ,
   PRIMARY KEY (`idtwitter_tweets`) )
 ENGINE = InnoDB;
 
@@ -50,78 +50,12 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `hottub`.`twitter_users_to_twitter_tweets`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `hottub`.`twitter_users_to_twitter_tweets` (
-  `twitter_users_idtwitter_users` BIGINT NOT NULL ,
-  `twitter_tweets_idtwitter_tweets` INT NOT NULL ,
-  PRIMARY KEY (`twitter_users_idtwitter_users`, `twitter_tweets_idtwitter_tweets`) ,
-  INDEX `fk_twitter_users_has_twitter_tweets_twitter_tweets1_idx` (`twitter_tweets_idtwitter_tweets` ASC) ,
-  INDEX `fk_twitter_users_has_twitter_tweets_twitter_users_idx` (`twitter_users_idtwitter_users` ASC) ,
-  CONSTRAINT `fk_twitter_users_has_twitter_tweets_twitter_users`
-    FOREIGN KEY (`twitter_users_idtwitter_users` )
-    REFERENCES `hottub`.`twitter_users` (`idtwitter_users` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_twitter_users_has_twitter_tweets_twitter_tweets1`
-    FOREIGN KEY (`twitter_tweets_idtwitter_tweets` )
-    REFERENCES `hottub`.`twitter_tweets` (`idtwitter_tweets` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `hottub`.`twitter_tweets_to_twitter_keywords`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `hottub`.`twitter_tweets_to_twitter_keywords` (
-  `twitter_tweets_idtwitter_tweets` INT NOT NULL ,
-  `twitter_keywords_idtwitter_keywords` INT NOT NULL ,
-  PRIMARY KEY (`twitter_tweets_idtwitter_tweets`, `twitter_keywords_idtwitter_keywords`) ,
-  INDEX `fk_twitter_tweets_has_twitter_keywords_twitter_keywords1_idx` (`twitter_keywords_idtwitter_keywords` ASC) ,
-  INDEX `fk_twitter_tweets_has_twitter_keywords_twitter_tweets1_idx` (`twitter_tweets_idtwitter_tweets` ASC) ,
-  CONSTRAINT `fk_twitter_tweets_has_twitter_keywords_twitter_tweets1`
-    FOREIGN KEY (`twitter_tweets_idtwitter_tweets` )
-    REFERENCES `hottub`.`twitter_tweets` (`idtwitter_tweets` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_twitter_tweets_has_twitter_keywords_twitter_keywords1`
-    FOREIGN KEY (`twitter_keywords_idtwitter_keywords` )
-    REFERENCES `hottub`.`twitter_keywords` (`idtwitter_keywords` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
 -- Table `hottub`.`twitter_hashtags`
 -- -----------------------------------------------------
 CREATE  TABLE IF NOT EXISTS `hottub`.`twitter_hashtags` (
   `idtwitter_hashtags` INT NOT NULL ,
   `twitter_hashtags_hashtag` VARCHAR(255) NULL ,
   PRIMARY KEY (`idtwitter_hashtags`) )
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `hottub`.`twitter_hashtags_to_twitter_tweets`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `hottub`.`twitter_hashtags_to_twitter_tweets` (
-  `twitter_hashtags_idtwitter_hashtags` INT NOT NULL ,
-  `twitter_tweets_idtwitter_tweets` INT NOT NULL ,
-  PRIMARY KEY (`twitter_hashtags_idtwitter_hashtags`, `twitter_tweets_idtwitter_tweets`) ,
-  INDEX `fk_twitter_hashtags_has_twitter_tweets_twitter_tweets1_idx` (`twitter_tweets_idtwitter_tweets` ASC) ,
-  INDEX `fk_twitter_hashtags_has_twitter_tweets_twitter_hashtags1_idx` (`twitter_hashtags_idtwitter_hashtags` ASC) ,
-  CONSTRAINT `fk_twitter_hashtags_has_twitter_tweets_twitter_hashtags1`
-    FOREIGN KEY (`twitter_hashtags_idtwitter_hashtags` )
-    REFERENCES `hottub`.`twitter_hashtags` (`idtwitter_hashtags` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_twitter_hashtags_has_twitter_tweets_twitter_tweets1`
-    FOREIGN KEY (`twitter_tweets_idtwitter_tweets` )
-    REFERENCES `hottub`.`twitter_tweets` (`idtwitter_tweets` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
@@ -136,11 +70,55 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
+-- Table `hottub`.`twitter_users_to_twitter_tweets`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `hottub`.`twitter_users_to_twitter_tweets` (
+  `twitter_users_idtwitter_users` BIGINT UNSIGNED NOT NULL ,
+  `twitter_tweets_idtwitter_tweets` BIGINT UNSIGNED NOT NULL ,
+  PRIMARY KEY (`twitter_users_idtwitter_users`, `twitter_tweets_idtwitter_tweets`) ,
+  INDEX `fk_twitter_users_has_twitter_tweets_twitter_tweets1_idx` (`twitter_tweets_idtwitter_tweets` ASC) ,
+  INDEX `fk_twitter_users_has_twitter_tweets_twitter_users1_idx` (`twitter_users_idtwitter_users` ASC) ,
+  CONSTRAINT `fk_twitter_users_has_twitter_tweets_twitter_users1`
+    FOREIGN KEY (`twitter_users_idtwitter_users` )
+    REFERENCES `hottub`.`twitter_users` (`idtwitter_users` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_twitter_users_has_twitter_tweets_twitter_tweets1`
+    FOREIGN KEY (`twitter_tweets_idtwitter_tweets` )
+    REFERENCES `hottub`.`twitter_tweets` (`idtwitter_tweets` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `hottub`.`twitter_users_twitter_mentions`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `hottub`.`twitter_users_twitter_mentions` (
+  `twitter_users_idtwitter_users` BIGINT UNSIGNED NOT NULL ,
+  `twitter_tweets_idtwitter_tweets` BIGINT UNSIGNED NOT NULL ,
+  PRIMARY KEY (`twitter_users_idtwitter_users`, `twitter_tweets_idtwitter_tweets`) ,
+  INDEX `fk_twitter_users_has_twitter_tweets1_twitter_tweets1_idx` (`twitter_tweets_idtwitter_tweets` ASC) ,
+  INDEX `fk_twitter_users_has_twitter_tweets1_twitter_users1_idx` (`twitter_users_idtwitter_users` ASC) ,
+  CONSTRAINT `fk_twitter_users_has_twitter_tweets1_twitter_users1`
+    FOREIGN KEY (`twitter_users_idtwitter_users` )
+    REFERENCES `hottub`.`twitter_users` (`idtwitter_users` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_twitter_users_has_twitter_tweets1_twitter_tweets1`
+    FOREIGN KEY (`twitter_tweets_idtwitter_tweets` )
+    REFERENCES `hottub`.`twitter_tweets` (`idtwitter_tweets` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
 -- Table `hottub`.`twitter_urls_to_twitter_tweets`
 -- -----------------------------------------------------
 CREATE  TABLE IF NOT EXISTS `hottub`.`twitter_urls_to_twitter_tweets` (
   `twitter_urls_idtwitter_urls` INT NOT NULL ,
-  `twitter_tweets_idtwitter_tweets` INT NOT NULL ,
+  `twitter_tweets_idtwitter_tweets` BIGINT UNSIGNED NOT NULL ,
   PRIMARY KEY (`twitter_urls_idtwitter_urls`, `twitter_tweets_idtwitter_tweets`) ,
   INDEX `fk_twitter_urls_has_twitter_tweets_twitter_tweets1_idx` (`twitter_tweets_idtwitter_tweets` ASC) ,
   INDEX `fk_twitter_urls_has_twitter_tweets_twitter_urls1_idx` (`twitter_urls_idtwitter_urls` ASC) ,
@@ -158,20 +136,42 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `hottub`.`twitter_users_to_twitter_mentions`
+-- Table `hottub`.`twitter_hashtags_to_twitter_tweets`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `hottub`.`twitter_users_to_twitter_mentions` (
-  `twitter_users_idtwitter_users` BIGINT NOT NULL ,
-  `twitter_tweets_idtwitter_tweets` INT NOT NULL ,
-  PRIMARY KEY (`twitter_users_idtwitter_users`, `twitter_tweets_idtwitter_tweets`) ,
-  INDEX `fk_twitter_users_has_twitter_tweets_twitter_tweets2_idx` (`twitter_tweets_idtwitter_tweets` ASC) ,
-  INDEX `fk_twitter_users_has_twitter_tweets_twitter_users1_idx` (`twitter_users_idtwitter_users` ASC) ,
-  CONSTRAINT `fk_twitter_users_has_twitter_tweets_twitter_users1`
-    FOREIGN KEY (`twitter_users_idtwitter_users` )
-    REFERENCES `hottub`.`twitter_users` (`idtwitter_users` )
+CREATE  TABLE IF NOT EXISTS `hottub`.`twitter_hashtags_to_twitter_tweets` (
+  `twitter_hashtags_idtwitter_hashtags` INT NOT NULL ,
+  `twitter_tweets_idtwitter_tweets` BIGINT UNSIGNED NOT NULL ,
+  PRIMARY KEY (`twitter_hashtags_idtwitter_hashtags`, `twitter_tweets_idtwitter_tweets`) ,
+  INDEX `fk_twitter_hashtags_has_twitter_tweets_twitter_tweets1_idx` (`twitter_tweets_idtwitter_tweets` ASC) ,
+  INDEX `fk_twitter_hashtags_has_twitter_tweets_twitter_hashtags1_idx` (`twitter_hashtags_idtwitter_hashtags` ASC) ,
+  CONSTRAINT `fk_twitter_hashtags_has_twitter_tweets_twitter_hashtags1`
+    FOREIGN KEY (`twitter_hashtags_idtwitter_hashtags` )
+    REFERENCES `hottub`.`twitter_hashtags` (`idtwitter_hashtags` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_twitter_users_has_twitter_tweets_twitter_tweets2`
+  CONSTRAINT `fk_twitter_hashtags_has_twitter_tweets_twitter_tweets1`
+    FOREIGN KEY (`twitter_tweets_idtwitter_tweets` )
+    REFERENCES `hottub`.`twitter_tweets` (`idtwitter_tweets` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `hottub`.`twitter_keywords_to_twitter_tweets`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `hottub`.`twitter_keywords_to_twitter_tweets` (
+  `twitter_keywords_idtwitter_keywords` INT NOT NULL ,
+  `twitter_tweets_idtwitter_tweets` BIGINT UNSIGNED NOT NULL ,
+  PRIMARY KEY (`twitter_keywords_idtwitter_keywords`, `twitter_tweets_idtwitter_tweets`) ,
+  INDEX `fk_twitter_keywords_has_twitter_tweets_twitter_tweets1_idx` (`twitter_tweets_idtwitter_tweets` ASC) ,
+  INDEX `fk_twitter_keywords_has_twitter_tweets_twitter_keywords1_idx` (`twitter_keywords_idtwitter_keywords` ASC) ,
+  CONSTRAINT `fk_twitter_keywords_has_twitter_tweets_twitter_keywords1`
+    FOREIGN KEY (`twitter_keywords_idtwitter_keywords` )
+    REFERENCES `hottub`.`twitter_keywords` (`idtwitter_keywords` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_twitter_keywords_has_twitter_tweets_twitter_tweets1`
     FOREIGN KEY (`twitter_tweets_idtwitter_tweets` )
     REFERENCES `hottub`.`twitter_tweets` (`idtwitter_tweets` )
     ON DELETE NO ACTION
